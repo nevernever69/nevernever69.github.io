481ffec89848eed49b2fa03c93b6d079ed48f708
nevernever69 <nevernevernever69@protonmail.com>
2024-04-15 22:24:14 +0530

    Fixes text-color

--- src/app/Sheet.jsx
+++ src/app/Sheet.jsx
@@ -20,13 +20,13 @@ const CSheet = () => {
 		</SheetTrigger>
 		<SheetContent  side="left" className="w-[200px] sm:w-[0px] h-screen">
 		<SheetHeader>
-		<SheetTitle>Page Content</SheetTitle>
+		<SheetTitle>Pages</SheetTitle>
 		<Separator/>
 		<SheetDescription>
 		<div className="py-8">
 		<ul className="font-bold text-lg flex flex-col ">
-		<Link to='/' ><SheetClose><li className="hover:text-white hover:bg-purple-600 hover:border-gray-300 py-4 "><p className="hover:text-red-600">About</p></li></SheetClose></Link>
-		<Link to='/blog'><SheetClose><li className=" hover:text-white hover:bg-purple-600 hover:border-gray-300 ">Blog</li></SheetClose></Link>
+		<Link to='/' ><SheetClose><li className=" hover:border-gray-700 py-4 underline hover:underline-offset-1 hover:text-red-700">About</li></SheetClose></Link>
+		<Link to='/blog'><SheetClose><li className=" hover:text-red-700 hover:border-gray-700 underline hover:underline-offset-1 ">Blog</li></SheetClose></Link>
 		</ul>
 		</div>
 		</SheetDescription>
