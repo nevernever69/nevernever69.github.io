ceb8c7512f224d57b35d0e7e11b1653f52440fae
nevernever69 <nevernevernever69@protonmail.com>
2024-04-15 22:46:59 +0530

    Added Section Connect

--- src/app/Content.jsx
+++ src/app/Content.jsx
@@ -1,4 +1,5 @@
-
+import Icon from '@mdi/react';
+import { mdiGithub,  mdiLinkedin, mdiTwitter } from '@mdi/js';
 const Content = () => {
 	return (
 		<>
@@ -16,9 +17,23 @@ const Content = () => {
 		<p>
 		Currently I'm working on <span className="font-bold">freeBSD </span>rc.conf to learn how it works. And unwraping the Underlying Concepts on <span className="font-bold">Machine Learning</span> and Reversing Some Binaries.
 		</p>
-		<p>
-			
+		<h2 className="py-4 text-gray-500 opacity-4 underline text-3xl font-bold">
+		#Intersted		
+		</h2>
+		<p className="py-2">
+		<p>- Operating System Development</p>
+		<p>- Exploit development</p>
+		<p>- Machine Learning</p>
 		</p>
+
+		<h2 className="py-4 text-gray-500 opacity-4 underline text-3xl font-bold">
+		#Connect
+		</h2>
+		<div className=" flex ">
+		<a className="px-2" href="https://github.com/nevernever69"><Icon path={mdiGithub} size={1} /></a>
+		<a className="px-2" href="https://linkedin.com/in/ashish-baghel-16a073230"><Icon path={mdiLinkedin} size={1} /></a>
+		<a className="px-2" href="https://twitter.com/Neverne55768089"><Icon path={mdiTwitter} size={1} /></a>
+		</div>
 		</div>
 		</>
 	)
